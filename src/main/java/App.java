5458ace5343f37d8644255490f1fb94cc52138f0
elena <elena.pro.68@gmail.com>
2024-02-15 14:48:26 +0300

    Cat bean added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+
+
+        Cat bean3 = (Cat) applicationContext.getBean("meow");
+        System.out.println(bean3.getVoice());
+        Cat bean4 = (Cat) applicationContext.getBean("meow");
+        System.out.println(bean4.getVoice());
+
+
     }
 }
